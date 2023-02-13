2897fcaf8f9019156254ab95c8570a1ecce601ef
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-13 15:56:34 +0100

    rut-token drop

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ import styles from "../styles/Home.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
 
 const Home = () => {
-  const tokenAddress = "0x03728725240b021887355c943d040BF933F3d5F0";
+  const tokenAddress = "0x021Ae3f484eb6D65B8fd6383391dFACAb41cea8f";
   const { contract } = useContract(tokenAddress, "token-drop");
   const address = useAddress();
   const [quantity, setQuantity] = useState(1);
